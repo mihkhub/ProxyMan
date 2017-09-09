84bd5f6e8ddfb78e920cd80fe8950a4558e4556e
Issac Chun <changfeng.or.chen79@gmail.com>
2017-09-09 23:56:07 +0800

    fixed gsettings path

--- gsettings.sh
+++ gsettings.sh
@@ -43,77 +43,79 @@
 
 # gettings : this is what Ubuntu and other GNOME based distributions use
 # for storing configuration
-# Your system settings UI uses gsettings at the backend.
+# Your system settings UI uses ${GSETTINGS} at the backend.
+
+GSETTINGS=/usr/bin/gsettings
 
 list_proxy() {
 	echo
 	echo -e "\e[1m Desktop proxy settings (GNOME)\e[0m"
-	mode="$(gsettings get org.gnome.system.proxy mode)"
+	mode="$(${GSETTINGS} get org.gnome.system.proxy mode)"
 	if [ "$mode" = "'none'" ]; then
 		echo -e "\e[36m None \e[0m"
 		return
 	fi
 
-	echo -e "\e[36m HTTP  Proxy \e[0m" Host : $(gsettings get org.gnome.system.proxy.http host) Port : $(gsettings get org.gnome.system.proxy.http port)
-	echo -e "\e[36m Auth        \e[0m" User  : $(gsettings get org.gnome.system.proxy.http authentication-user) Password : $(gsettings get org.gnome.system.proxy.http authentication-password)
-	echo -e "\e[36m HTTPS Proxy \e[0m" Host : $(gsettings get org.gnome.system.proxy.https host) Port : $(gsettings get org.gnome.system.proxy.https port)
-	echo -e "\e[36m FTP   Proxy \e[0m" Host : $(gsettings get org.gnome.system.proxy.ftp host) Port : $(gsettings get org.gnome.system.proxy.ftp port)
-	echo -e "\e[36m SOCKS Proxy \e[0m" Host : $(gsettings get org.gnome.system.proxy.socks host) Port : $(gsettings get org.gnome.system.proxy.socks port)
+	echo -e "\e[36m HTTP  Proxy \e[0m" Host : $(${GSETTINGS} get org.gnome.system.proxy.http host) Port : $(${GSETTINGS} get org.gnome.system.proxy.http port)
+	echo -e "\e[36m Auth        \e[0m" User  : $(${GSETTINGS} get org.gnome.system.proxy.http authentication-user) Password : $(${GSETTINGS} get org.gnome.system.proxy.http authentication-password)
+	echo -e "\e[36m HTTPS Proxy \e[0m" Host : $(${GSETTINGS} get org.gnome.system.proxy.https host) Port : $(${GSETTINGS} get org.gnome.system.proxy.https port)
+	echo -e "\e[36m FTP   Proxy \e[0m" Host : $(${GSETTINGS} get org.gnome.system.proxy.ftp host) Port : $(${GSETTINGS} get org.gnome.system.proxy.ftp port)
+	echo -e "\e[36m SOCKS Proxy \e[0m" Host : $(${GSETTINGS} get org.gnome.system.proxy.socks host) Port : $(${GSETTINGS} get org.gnome.system.proxy.socks port)
 
 }
 
 toggle_proxy() {
-	if [ "$(gsettings get org.gnome.system.proxy mode)" = "none" ]; then
-		gsettings set org.gnome.system.proxy mode "manual"
+	if [ "$(${GSETTINGS} get org.gnome.system.proxy mode)" = "none" ]; then
+		${GSETTINGS} set org.gnome.system.proxy mode "manual"
 	else
-		gsettings set org.gnome.system.proxy mode "none"
+		${GSETTINGS} set org.gnome.system.proxy mode "none"
 	fi
 }
 
 unset_proxy() {
-	gsettings set org.gnome.system.proxy mode "none"
-	gsettings set org.gnome.system.proxy.http host \"\"
-	gsettings set org.gnome.system.proxy.http port 0
-	gsettings set org.gnome.system.proxy.https host "\"\""
-	gsettings set org.gnome.system.proxy.https port 0
-	gsettings set org.gnome.system.proxy.ftp host "\"\""
-	gsettings set org.gnome.system.proxy.ftp port 0
-	gsettings set org.gnome.system.proxy.socks host "\"\""
-	gsettings set org.gnome.system.proxy.socks port 0
-	gsettings set org.gnome.system.proxy.http use-authentication false
-	gsettings set org.gnome.system.proxy.http authentication-user "\"\""
-	gsettings set org.gnome.system.proxy.http authentication-password "\"\""
+	${GSETTINGS} set org.gnome.system.proxy mode "none"
+	${GSETTINGS} set org.gnome.system.proxy.http host \"\"
+	${GSETTINGS} set org.gnome.system.proxy.http port 0
+	${GSETTINGS} set org.gnome.system.proxy.https host "\"\""
+	${GSETTINGS} set org.gnome.system.proxy.https port 0
+	${GSETTINGS} set org.gnome.system.proxy.ftp host "\"\""
+	${GSETTINGS} set org.gnome.system.proxy.ftp port 0
+	${GSETTINGS} set org.gnome.system.proxy.socks host "\"\""
+	${GSETTINGS} set org.gnome.system.proxy.socks port 0
+	${GSETTINGS} set org.gnome.system.proxy.http use-authentication false
+	${GSETTINGS} set org.gnome.system.proxy.http authentication-user "\"\""
+	${GSETTINGS} set org.gnome.system.proxy.http authentication-password "\"\""
 }
 
 set_proxy() {
 	if [ "$4" = "y" ]; then
-		gsettings set org.gnome.system.proxy.http authentication-user "$5"
-		gsettings set org.gnome.system.proxy.http authentication-password "$6"
+		${GSETTINGS} set org.gnome.system.proxy.http authentication-user "$5"
+		${GSETTINGS} set org.gnome.system.proxy.http authentication-password "$6"
 	fi
 	if [ "$3" = "y" ]; then
-		gsettings set org.gnome.system.proxy.http host $1
-		gsettings set org.gnome.system.proxy.http port $2
-		gsettings set org.gnome.system.proxy.https host $1
-		gsettings set org.gnome.system.proxy.https port $2
-		gsettings set org.gnome.system.proxy.ftp host $1
-		gsettings set org.gnome.system.proxy.ftp port $2
-		gsettings set org.gnome.system.proxy.socks host $1
-		gsettings set org.gnome.system.proxy.socks port $2
+		${GSETTINGS} set org.gnome.system.proxy.http host $1
+		${GSETTINGS} set org.gnome.system.proxy.http port $2
+		${GSETTINGS} set org.gnome.system.proxy.https host $1
+		${GSETTINGS} set org.gnome.system.proxy.https port $2
+		${GSETTINGS} set org.gnome.system.proxy.ftp host $1
+		${GSETTINGS} set org.gnome.system.proxy.ftp port $2
+		${GSETTINGS} set org.gnome.system.proxy.socks host $1
+		${GSETTINGS} set org.gnome.system.proxy.socks port $2
 	elif [ "$3" = "n" ]; then
-		gsettings set org.gnome.system.proxy.http host $1
-		gsettings set org.gnome.system.proxy.http port $2
-		gsettings set org.gnome.system.proxy.https host $7
-		gsettings set org.gnome.system.proxy.https port $8
-		gsettings set org.gnome.system.proxy.ftp host $9
-		gsettings set org.gnome.system.proxy.ftp port $10
-		gsettings set org.gnome.system.proxy.socks host $9
-		gsettings set org.gnome.system.proxy.socks port $10
+		${GSETTINGS} set org.gnome.system.proxy.http host $1
+		${GSETTINGS} set org.gnome.system.proxy.http port $2
+		${GSETTINGS} set org.gnome.system.proxy.https host $7
+		${GSETTINGS} set org.gnome.system.proxy.https port $8
+		${GSETTINGS} set org.gnome.system.proxy.ftp host $9
+		${GSETTINGS} set org.gnome.system.proxy.ftp port $10
+		${GSETTINGS} set org.gnome.system.proxy.socks host $9
+		${GSETTINGS} set org.gnome.system.proxy.socks port $10
 	fi
-	gsettings set org.gnome.system.proxy mode "manual"
+	${GSETTINGS} set org.gnome.system.proxy mode "manual"
 }
 
 
-gsettings_available="$(which gsettings)"
+gsettings_available="$(which ${GSETTINGS})"
 if [ "$gsettings_available" = "" ]; then
 	exit
 fi
